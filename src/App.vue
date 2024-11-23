7218f562613bde74de36c479004ae7770b8582aa
DantersParra <oreomcdante@gmail.com>
2024-11-23 16:55:27 +0800

    Tailwind, primeicon, heroicon installation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
-
+import { ChatBubbleOvalLeftIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
 
-  
+  <div class="px-[10rem]">
+    <div class="flex">
+      <p>Chat bubble</p>
+      <ChatBubbleOvalLeftIcon class="size-6 ml-2" />
+    </div>
+    
+  </div>
+
 </template>
 
 <style scoped>
